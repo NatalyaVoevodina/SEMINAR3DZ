690832087d656abd19b7317919114a326c239780
natlya_voevodina <voevodochka1988@yandex.ru>
2023-02-23 17:15:36 +0300

    task3.3

---
+++ task3.3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for(int i=1; i<=N; i++)
+{
+int res = (int)Math.Pow(i, (1 * 3));
+Console.Write($"{res}  ");
+}
